bbb48539d1641a7095a307c38ab7bce5b6a5b479
yukihane <yukihane.feather@gmail.com>
2021-12-23 03:00:12 +0900

    p.37 2.3.2 when を使って enum クラスを処理する

---
+++ kotlin-in-action/ch02/app/src/main/kotlin/ch02/Color.kt
@@ -0,0 +1,26 @@
+package ch02
+
+enum class Color {
+    RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET
+}
+
+fun getMnemonic(color: Color) = when (color) {
+    Color.RED -> "Richard"
+    Color.ORANGE -> "Of"
+    Color.YELLOW -> "York"
+    Color.GREEN -> "Gave"
+    Color.BLUE -> "Battle"
+    Color.INDIGO -> "In"
+    Color.VIOLET -> "Vain"
+}
+
+fun getWarmth(color: Color) = when (color) {
+    Color.RED, Color.ORANGE, Color.YELLOW -> "warm"
+    Color.GREEN -> "neutral"
+    Color.BLUE, Color.INDIGO, Color.VIOLET -> "cold"
+}
+
+fun main() {
+    println(getMnemonic(Color.YELLOW))
+    println(getWarmth(Color.ORANGE))
+}
