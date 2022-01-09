6a464734e7278022322913223c8c1525b784141d
yukihane <yukihane.feather@gmail.com>
2022-01-10 02:07:29 +0900

    init

---
+++ junit4-mockito-example/src/main/kotlin/com/github/yukihane/example/kotlin/Hello.kt
@@ -0,0 +1,6 @@
+package com.github.yukihane.example.kotlin
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ junit4-mockito-example/src/test/kotlin/com/github/yukihane/example/kotlin/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.github.yukihane.example.kotlin
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
