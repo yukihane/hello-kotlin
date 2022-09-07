a4520f09eba973ef28ee50e46924f5fc041d35be
yukihane <yukihane.feather@gmail.com>
2022-09-08 08:34:37 +0900

    java interface

---
+++ java-implements-kotlin-interface/src/main/java/IPerson.java
@@ -0,0 +1,4 @@
+public interface IPerson {
+    int getAge();
+    String getName();
+}
---
+++ java-implements-kotlin-interface/src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val p1 = Person().apply {
+        name = "Bob"
+        age = 20
+    }
+}
